d8d87487164ebd4161c1c20b4d4e26892bd4ec18
João Fernandes <joaofnds@joaofnds.com>
2020-08-26 16:39:03 -0300

    Complete dice project

--- koans/about_dice_project.py
+++ koans/about_dice_project.py
@@ -16,7 +16,7 @@ class DiceSet:
     def roll(self, n):
         # Needs implementing!
         # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = [random.randint(1, 6) for _ in range(0, n)]
 
 class AboutDiceProject(Koan):
     def test_can_create_a_dice_set(self):
